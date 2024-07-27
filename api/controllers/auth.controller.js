2782a99cab5863212232b1e91d2e692a702b4301
Ariel Brendalsmo Orellana <arielbo@ulrik.uio.no>
2024-07-27 15:30:56 +0800

    created and added google oauth component

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-{/* Method to sign up and create new user with hashed password */}
+//Method to sign up and create new user with hashed password
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -16,7 +16,7 @@ export const signup = async (req, res, next) => {
     }
 };
 
-{/* Method to check user credentials and creating secret key */}
+//Method to check user credentials and creating secret key
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
@@ -29,5 +29,37 @@ export const signin = async (req, res, next) => {
         res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest); {/* Creating a cookie for the users session */}
     } catch (error) {
         next(error);
+    }
+}
+
+//Method to create user for Google Auth
+export const google = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = user._doc;
+            res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+        } else {
+            //If user does not exist, create a password because it is required
+            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), 
+                email: req.body.email, 
+                password: hashedPassword, 
+                avatar: req.body.photo });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = newUser._doc;
+            res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+        }
+    } catch (error) {
+        next(error)
     }
 }
