f2309b4101c1fba628558098ff78a5fc391f2e97
Ariel Brendalsmo Orellana <arielbo@ulrik.uio.no>
2024-11-06 18:43:11 +0800

    created get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
     res.json ({
@@ -17,4 +18,20 @@ export const deleteUser = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
+
+{/* Gets the listings of the user */}
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id){
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error)
+        }
+
+    } else {
+        return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+   
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { deleteUser, test } from '../controllers/user.controller.js';
+import { getUserListings, deleteUser, test } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
