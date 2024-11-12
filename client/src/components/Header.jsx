ef51a4814c02dae85bc822311fcba266cce9de96
Ariel Brendalsmo Orellana <arielbo@ulrik.uio.no>
2024-11-12 17:24:03 +0800

    created search bar and added functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { FaSearch } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const {currentUser} = useSelector(state => state.user)
+  const {currentUser} = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
-  {/* Header with links to different pages */}
+  {/* Function for handling the submition in the search bar */}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search); //Getting the URL with the built in method
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString(); 
+    navigate(`/search?${searchQuery}`);
+  };
+
+  {/* Getting the searchTerm from the URL and setting it in the search bar */}
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  {/* Header with links to different pages and search bar */}
   return (
     <header className='bg-white shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -17,6 +39,22 @@ export default function Header() {
           <li className='hover:underline'>Buy</li>
           <li className='hover:underline'>Rent</li>
         </ul>
+
+        <form onSubmit={handleSubmit} className='border rounded-3xl p-2 pl-3 flex items-center shadow-lg'>
+          <input 
+            type='text' 
+            placeholder='Search...' 
+            className='bg-transparent focus: outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <div className="flex items-center justify-center rounded-full bg-green-dark p-2">
+            <button>
+              <FaSearch className='w-3 h-3 text-white' />
+            </button>
+          </div>
+        </form>
+
         <ul className='flex gap-4'>
           <Link to='/'>
             <li className='max-sm:hidden sm_inline hover:underline'>Home</li>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green': {
+          'dark': '#1A4D2E',
+          'light': '#4F6F52',
+        },
+        'beige': {
+          'dark': '#E8DFCA',
+          'light': '#F5EFE6',
+        },
+      },
+    },
   },
   plugins: [require('tailwindcss-motion')],
 }
